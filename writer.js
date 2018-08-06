e6dee60931c78ec61018fea6189ac51f2b411b60
Travis Fantina <tfantina@gmail.com>
2018-08-06 09:27:09 -0400

    Working with safari

--- writer.js
+++ writer.js
@@ -7,6 +7,15 @@ let blurId = document.getElementById("written-blur");
 //Starts focus in the "writer id" focus can be regained by clicking in the "content id"
 document.addEventListener("DOMContentLoaded", function() {
   writerId.focus()
+
+
+  var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+  var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || safari.pushNotification);
+
+  console.log(isSafari);
+  if(isSafari) {
+    console.log('safari');
+  }
 });
 
 document.getElementById("container").addEventListener("click", function() {
@@ -24,13 +33,17 @@ writerId.addEventListener("paste", function(e) {
 //Event listner for keystrokes if the "writer" gets larger than 570 or the user
 //presses enter a new line is started.
 writerId.addEventListener('keydown', function(event){
-  event.preventDefault();
+
 
   if(event.key === "Enter" || document.getElementById("writer").offsetWidth >= "570") {
 
-    console.log("enter");
     if(initialState === true) {
+
       writtingContent = writerId.innerHTML;
+    /*  if(navigator.userAgent.indexOf("Safari") != -1) {
+        writtingContent = writtingContent + "</br>";
+        console.log(navigator.userAgent);
+      } */
       blurId.innerHTML = writtingContent ;
       writerId.innerHTML = "";
       initialState = false;
